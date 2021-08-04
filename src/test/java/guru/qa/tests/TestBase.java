e0b1808018f7fc18e8f61c3f45661bc5ad5e89e8
SBorisoglebsky <SBorisoglebsky@gmail.com>
2021-08-04 22:19:48 +0300

    fix remote driver

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-       // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
